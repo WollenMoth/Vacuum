85cb89462937b8e5e569c47f249e762f5ced37cc
André Díaz <andre290800@gmail.com>
2022-08-18 13:59:06 -0500

    Define Basic Operation

--- Vacuum.cpp
+++ Vacuum.cpp
@@ -1,5 +1,62 @@
 #include "Arduino.h"
 #include "Vacuum.h"
 
+#define LEFT false
+#define RIGHT true
+#define BACKWARD false
+#define FORWARD true
+
 Vacuum::Vacuum() {
+}
+
+void _startEngine(bool side, bool direction) {
+  switch(side) {
+    case LEFT:
+      break;
+    case RIGHT:
+      break;
+  }
+}
+
+void _stopEngine(bool side) {
+  switch(side) {
+    case LEFT:
+      break;
+    case RIGHT:
+      break;
+  }
+}
+
+void Vacuum::move(bool direction) {
+  _startEngine(LEFT, direction);
+  _startEngine(RIGHT, direction);
+}
+
+void Vacuum::turn(bool side) {
+  switch (side) {
+    case LEFT:
+      _startEngine(LEFT, BACKWARD);
+      _startEngine(RIGHT, FORWARD);
+      break;
+    case RIGHT:
+      _startEngine(LEFT, FORWARD);
+      _startEngine(RIGHT, BACKWARD);
+      break;
+  }
+}
+
+void Vacuum::stop() {
+  _stopEngine(LEFT);
+  _stopEngine(RIGHT);
+}
+
+void Vacuum::cleanTrash() {
+}
+
+bool Vacuum::senseTrash() {
+  return false;
+}
+
+int Vacuum::senseObstacles() {
+  return 0;
 }
--- Vacuum.h
+++ Vacuum.h
@@ -4,8 +4,15 @@
 
 class Vacuum {
   private:
+    void _startEngine(bool side, bool direction);
   public:
     Vacuum();
+    void move(bool direction);
+    void turn(bool side);
+    void stop();
+    void cleanTrash();
+    bool senseTrash();
+    int senseObstacles();
 };
 
 #endif
